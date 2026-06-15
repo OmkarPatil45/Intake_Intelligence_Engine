42f0fe42faa55acaaa7d4ce984a444d8e992755e
Omkar Patil <ompatil5132@gmail.com>
2026-06-15 11:53:24 +0530

    main

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+# from intake.intake_loader import IntakeLoader
+
+# loader = IntakeLoader()
+
+# result = loader.load(
+#     "dataset/Data.csv"
+# )
+
+# print(result)
+
 from intake.intake_loader import IntakeLoader
+from analysis.content_analyzer import ContentAnalyzer
 
 loader = IntakeLoader()
+analyzer = ContentAnalyzer()
 
-result = loader.load(
-    "dataset/Data.csv"
-)
+result = loader.load("dataset/resume.pdf")
+content = result["extraction"]["content"]
 
-print(result)
+analysis_result = analyzer.analyze(content)
+
+print(analysis_result)

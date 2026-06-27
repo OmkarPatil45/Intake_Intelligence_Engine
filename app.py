748550b8aa47d0ad25f5458cde6f0d2738c6b2b5
Omkar Patil <ompatil5132@gmail.com>
2026-06-27 23:04:30 +0530

    main

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from processing.processing_trace import ProcessingTrace
 
 
 loader = IntakeLoader()
-result = loader.load("dataset/user_device_.csv")
+result = loader.load("dataset/BI_REPORT.pdf")
 
 loader = IntakeLoader()
 analyzer = ContentAnalyzer()
